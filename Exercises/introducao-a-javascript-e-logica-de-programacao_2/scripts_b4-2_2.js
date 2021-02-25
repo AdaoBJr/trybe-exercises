523873caf55b27e140ecfb52a73930ff42699d29
Adão Benites <adaobjr@gmail.com>
2021-02-25 16:59:22 -0400

    Adiciona Exercicio 2

---
+++ Exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts_b4-2_2.js
@@ -0,0 +1,17 @@
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//for (let i=0; i < numbers.length;i++){
+//    console.log(numbers[i])
+//}
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0
+
+for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+    soma = soma + numbers[indexNumbers]
+
+      
+}
+
+console.log (soma);
+
