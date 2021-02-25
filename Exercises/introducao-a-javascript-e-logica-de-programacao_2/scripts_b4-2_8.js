8080c03b82d30f8593c3b3673099b4e1f43a8be0
Adão Benites <adaobjr@gmail.com>
2021-02-25 19:51:47 -0400

    Adicionado Exercício 6

--- Exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts_b4-2_8.js
+++ Exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts_b4-2_8.js
@@ -2,8 +2,8 @@
 //Exercício 1
 //let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//for (let i=0; i < numbers.length;i++){
-//    console.log(numbers[i])
+//for (let index=0; index < numbers.length;index++){
+   // console.log(numbers[index])
 //}
 
 //Exercício 2
@@ -61,7 +61,7 @@
 //let maior = 0
 
 //for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
-   //if ( numbers[indexNumbers] > maior) {
+   //if (numbers[indexNumbers] > maior) {
        //maior = numbers[indexNumbers];
    //}
      
@@ -71,29 +71,46 @@
 
 
     //Exercício 6
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let isNoEven = 0;
-let isEven = 0;
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+   //for (let numberImpar=0; )
+     
+//}
+    //console.log (maior);
 
 
 
-for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
-    if (numbers[indexNumbers]%2 == 0) {
-        isEven++;
 
-    } else {
-        isNoEven++;
-    }
-   
-   }
+       //Exercício 7
+       //let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+       //let menor = 0
+       
+       //for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+          //if (numbers[indexNumbers] < menor) {
+              //menor = numbers[indexNumbers];
+          //}
+            
+       //}
+           //console.log (menor)
 
-   if (isNoEven == 0) {
-       console.log ("Nenhum valor ímpar encontrado.")
-   } else {
-       console.log(isNoEven + " encontrado(s).")
-   }
+
+
+           //Exercício 8
+       let myArray = [];
     
+              
+       for (let indexMyArray=0; indexMyArray < 25; indexMyArray++)  {
+          if (myArray[indexMyArray] < 25) {
+              myArray.push(indexMyArray);
+          }
+            
+       }
+           console.log (myArray);
     
+
+
 
 
 
