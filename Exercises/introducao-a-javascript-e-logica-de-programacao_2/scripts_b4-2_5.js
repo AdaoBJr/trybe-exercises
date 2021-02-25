3a40e5874e2a0770f6e6b821b35000485d3fb70a
Adão Benites <adaobjr@gmail.com>
2021-02-25 17:45:11 -0400

    Adicionado Exercicio 5

---
+++ Exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts_b4-2_5.js
@@ -0,0 +1,76 @@
+
+//Exercício 1
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//for (let i=0; i < numbers.length;i++){
+//    console.log(numbers[i])
+//}
+
+//Exercício 2
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+    //soma = soma + numbers[indexNumbers]
+
+      
+//}
+
+//console.log (soma);
+
+
+//Exercício 3
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0
+//let media = 0
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+    //soma = soma + numbers[indexNumbers]
+    //media = soma/numbers.length
+
+      
+//}
+
+//console.log (media);
+
+
+
+//Exercício 4
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0
+//let media = 0
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+    //soma = soma + numbers[indexNumbers]
+    //media = soma/numbers.length
+     
+//}
+
+//if (media > 20) {
+    //console.log (media);
+    //console.log ("Valor maior que 20");
+
+//} else {
+    //console.log (media);
+    //console.log ("Valor menor que 20");
+//}
+
+
+//Exercício 5
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = 0
+
+for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+   if ( numbers[indexNumbers] > maior) {
+       maior = numbers[indexNumbers];
+   }
+     
+}
+    console.log (maior);
+    
+
+
+
+
+
+
