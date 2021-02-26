7c635e59ea0d9294812585c4e37a589d7192d6c1
Adão Benites <adaobjr@gmail.com>
2021-02-25 20:24:49 -0400

    Adicionado Exercício 9  e comentário com o enunciado de cada exercício

---
+++ Exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts_b4-2_9.js
@@ -0,0 +1,172 @@
+
+//Exercício 1
+//Nesse primeiro exercício, percorra o array 
+//imprimindo todos os valores nele contidos com a função console.log() ; 
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//for (let index=0; index < numbers.length;index++){
+   // console.log(numbers[index])
+//}
+
+//Exercício 2
+//Para o segundo exercício, você deve somar todos os 
+//valores contidos no array e imprimir o resultado; 
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+    //soma = soma + numbers[indexNumbers]
+
+      
+//}
+
+//console.log (soma);
+
+
+//Exercício 3
+//Para o terceiro exercício, calcule e imprima a média 
+//aritmética dos valores contidos no array; 
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0
+//let media = 0
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+    //soma = soma + numbers[indexNumbers]
+    //media = soma/numbers.length
+
+      
+//}
+
+//console.log (media);
+
+
+
+//Exercício 4
+//Com o mesmo código do exercício anterior, caso valor final seja maior que 20, 
+//imprima a mensagem: "valor maior que 20". Caso não seja, imprima a 
+//mensagem: "valor menor ou igual a 20"; 
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let soma = 0
+//let media = 0
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+    //soma = soma + numbers[indexNumbers]
+    //media = soma/numbers.length
+     
+//}
+
+//if (media > 20) {
+    //console.log (media);
+    //console.log ("Valor maior que 20");
+
+//} else {
+    //console.log (media);
+    //console.log ("Valor menor que 20");
+//}
+
+
+//Exercício 5
+//Utilizando for , descubra qual o maior valor contido 
+//no array e imprima-o;
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let maior = 0
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+   //if (numbers[indexNumbers] > maior) {
+       //maior = numbers[indexNumbers];
+   //}
+     
+//}
+    //console.log (maior);
+
+
+
+//Exercício 6
+//Descubra quantos valores ímpares existem no array e 
+//imprima o resultado. Caso não exista nenhum, imprima a 
+//mensagem: "nenhum valor ímpar encontrado"; 
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let isNoEven = 0;
+//let isEven = 0;
+
+
+
+//for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+    //if (numbers[indexNumbers]%2 == 0) {
+        //isEven++;
+
+    //} else {
+       // isNoEven++;
+   // }
+   
+  // }
+
+  // if (isNoEven == 0) {
+  //     console.log ("Nenhum valor ímpar encontrado.")
+  // } else {
+  //     console.log(isNoEven + " encontrado(s).")
+  // }
+
+
+  //Exercício 7
+  //Utilizando for , descubra qual o menor valor contido 
+  //no array e imprima-o;
+
+  //let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  //let menor = 0
+  
+  //for (let indexNumbers=0; indexNumbers < numbers.length; indexNumbers++)  {
+   //  if (numbers[indexNumbers] < menor) {
+    //     menor = numbers[indexNumbers];
+    // }
+       
+  //}
+   //   console.log (menor)
+    
+
+
+
+//Exercício 8
+//Utilizando for , crie uma array que vá de 1 até 25 e 
+//imprima o resultado; 
+
+       //let myArray = [];
+    
+              
+      // for (let indexMyArray=1; indexMyArray <= 25; indexMyArray++)  {
+       //   myArray.push(indexMyArray);
+          
+      // }
+       //    console.log (myArray);
+
+
+
+
+//Exercício 9
+//Utilizando o array criado no exercício anterior 
+//imprima o resultado da divisão de cada um dos elementos por 2 .
+
+       let myArray = [];
+         
+              
+       for (let indexMyArray=1; indexMyArray <= 25; indexMyArray++)  {
+          myArray.push(indexMyArray/2);
+                             
+       }    
+       
+           console.log (myArray);
+    
+
+
+
+
+
+
+
+
