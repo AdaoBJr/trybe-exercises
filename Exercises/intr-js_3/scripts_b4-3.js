48b1ec50d7fdca06b5410e40d55160c63fa631c3
Adão Benites <adaobjr@gmail.com>
2021-02-26 18:04:53 -0400

    Ajustado Exerc 1 e resolvido Exerc 2

--- Exercises/intr-js_3/scripts_b4-3.js
+++ Exercises/intr-js_3/scripts_b4-3.js
@@ -11,15 +11,42 @@
 //*****
 //*****
 
-let n = 4;
-let ast = "";
+//let n = 4;
+//let ast = "*";
+//let lin ="";
 
-for (let linhas = 1; linhas <= n; linhas++){
-    ast = ast + "*";
+//for (let indexLin = 1; indexLin <= n; indexLin++){
+//    lin = lin + ast;
+//    }
+
+//    console.log(lin)
+
+//for ( let imp = 1; imp <= n; imp++){
+//   console.log(lin);
+//}
+
+
+//Exercício 2
+//Para o segundo exercício, faça o mesmo que antes, mas que imprima um 
+//triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+//n = 5
+
+//*
+//**
+//***
+//****
+//*****
+
+
+let base = 5;
+let ast = "*";
+let lin = "";
+
+for (let indexLin = 0; indexLin <= base; indexLin++){
+    lin = lin + ast;
+
+    console.log(lin)
+
     }
-    console.log(ast)
-
-for ( let imp = 1; imp <= n; imp++){
-    console.log(ast);
-}
-
+    
