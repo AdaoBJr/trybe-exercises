2079c4da9610802f385aba128d67c85344287900
Adão Benites <adaobjr@gmail.com>
2021-03-01 15:50:26 -0400

    Adicionado exercícios-conteudo aula 4 - For in - JS

--- Exercises/intr-js_4/scripts-conteudo.js
+++ Exercises/intr-js_4/scripts-conteudo.js
@@ -1,7 +1,6 @@
 let player = {
     name : "Marta",
     lastName : "Silva",
-    fullName : "Marta Silva",
     age : 34,
     medals : {
         golden : 2,
@@ -9,6 +8,7 @@ let player = {
     }
 };
 player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+player.fullName = player.name + " " + player.lastName;
 
 //Verificando o Objeto em formato Table
 //console.table(player);
@@ -19,4 +19,21 @@ player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 
 
 //Solicitação 02
-console.log("A jogadora " + player.fullName + " " + "foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " anos.");
+//console.log("A jogadora " + player.fullName + " " + "foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " anos.");
+
+
+//For in com Arrays e Objetos
+//let cars = ["Volvo", "Tesla", "GM"];
+//for (let index in cars) {
+//    console.log(cars[index]);
+//}
+
+let car = {
+    marca : "Tesla",
+    modelo : "Model S",
+    cor : "Vermelho"
+};
+
+for (let index in car){
+    console.log(index, car[index]);
+}
