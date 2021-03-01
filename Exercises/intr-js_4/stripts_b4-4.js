e7de113fdd11b7d9dcb3e567774b0323bd1d96d4
Adão Benites <adaobjr@gmail.com>
2021-03-01 19:03:46 -0400

    Adicionado teste de entendimento do algoritmo do Exercício 1

--- Exercises/intr-js_4/stripts_b4-4.js
+++ Exercises/intr-js_4/stripts_b4-4.js
@@ -1,3 +1,4 @@
+//FOR IN
 //Exercício 1
 //Imprima no console uma mensagem de boas-vindas para a personagem acima, 
 //incluindo seu nome.
@@ -32,17 +33,91 @@
 //Faça um for/in que mostre todas as chaves do objeto. 
 
 
-let info = {
-    personagem: "Margarida",
-    origem: "Pato Donald",
-    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  };
+    //let info = {
+      //  personagem: "Margarida",
+      //  origem: "Pato Donald",
+       // nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  //  };
 
-  info.recorrente = "Sim";
+    //info.recorrente = "Sim";
 
-  for (let key in info){
-      console.log(key);
+    //for (let key in info){
+    //    console.log(key);
+    //}
+
+
+   //Exercício 4
+//Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. 
+
+
+//let info = {
+  //  personagem: "Margarida",
+  //  origem: "Pato Donald",
+  //  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+ // };
+
+ // info.recorrente = "Sim";
+
+  //for (let key in info){
+ //     console.log(info[key]);
+ // }
+
+  //Exercício 5
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) 
+//do primeiro e os seguintes 
+//valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+
+
+//let info = {
+  //  personagem: "Margarida",
+  //  origem: "Pato Donald",
+  //  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  //  recorrente = "Sim"
+  //};
+
+ 
+
+  //let info2 = {
+  //  personagem: "Tio Patinhas",
+  //  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  //  nota: "O último MacPatinhas",
+  //  recorrente = "Sim"
+ // };
+
+  //for (let key in info){
+    //  for (let key2 in info2){
+
+     // }
+     // console.log(info[key] + " e " + info[key2]);
+  //}
+ 
+//-------------------------------------------------------------------------------------
+//FUNCTION
+
+let palavra = "arara";
+let arrayLetras = palavra.split("");
+let isPa = false;
+
+//for ( let index = 0; index < arrayLetras.length; index++){
+//  console.log(index, arrayLetras[index]);
+//}
+
+//for ( let index in arrayLetras){
+ // console.log(index, arrayLetras[index]);
+//}
+
+for (let index = 0; index < arrayLetras.length; index++){
+  if (arrayLetras[index] == arrayLetras[(arrayLetras.length-1) - index]){
+  isPa = true;
+} else {
   }
+}
+   
+console.log(isPa);
+
+      
+       
+ 
 
 
   
