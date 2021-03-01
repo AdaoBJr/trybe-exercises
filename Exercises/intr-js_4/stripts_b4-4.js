10d67d6ed027dabe2d2e1e393eb98a69c02c2034
Adão Benites <adaobjr@gmail.com>
2021-03-01 16:11:36 -0400

    Adiciona Exercício 2 - Aula 4

--- Exercises/intr-js_4/stripts_b4-4.js
+++ Exercises/intr-js_4/stripts_b4-4.js
@@ -3,10 +3,26 @@
 //incluindo seu nome.
 
 
+//let info = {
+    //personagem: "Margarida",
+   // origem: "Pato Donald",
+  //  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  //};
+
+//  console.log("Bem-vinda, " + info.personagem);
+
+
+  //Exercício 2
+//Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o 
+//valor "Sim" e, em seguida, imprima o objeto no console. 
+
+
 let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   };
 
-  console.log("Bem-vinda, " + info.personagem);
+  info.recorrente = "Sim";
+
+  console.table(info);
